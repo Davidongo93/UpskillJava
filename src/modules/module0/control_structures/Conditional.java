1764c0abb3082e79a8e3637c6c2b399134e49728
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-10-27 16:28:31 -0500

    module and package imported switch pending

--- src/modules/module0/control_structures/Conditional.java
+++ src/modules/module0/control_structures/Conditional.java
@@ -1,12 +1,19 @@
 package modules.module0.control_structures;
 
 public class Conditional{
-    public void execIfElse(){
+    public static void execIfElse(){
         int number = 7;
         if (number != 2){
             System.out.println("No es el numero dos");
         } else {
             System.out.println("Escribiste un dos, sos un capo");
         };
+    };
+    public void execTernary(){
+        int number = 7;
+        String result = (number % 2 == 0)
+                ? "Its pair"
+                : "its not pair";
+        System.out.println(result + " the numbar");
+        };
     }
-}
