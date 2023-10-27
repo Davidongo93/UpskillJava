cf8821cd7658d79fd9e367e8ccc19e182801d5fa
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-10-27 16:07:52 -0500

    A unique repository 4 all course.

---
+++ src/modules/module0/control_structures/Conditional.java
@@ -0,0 +1,12 @@
+package modules.module0.control_structures;
+
+public class Conditional{
+    public void execIfElse(){
+        int number = 7;
+        if (number != 2){
+            System.out.println("No es el numero dos");
+        } else {
+            System.out.println("Escribiste un dos, sos un capo");
+        };
+    }
+}
