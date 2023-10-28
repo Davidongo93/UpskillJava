7d770d512dbd3a728a88966a0c106fa521251ff6
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-10-27 19:07:51 -0500

    ternary module

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import static modules.module0.control_structures.Conditional.execIfElse;
-
+import static modules.module0.control_structures.Conditional.execTernary;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Holi mundo");
         execIfElse();
+        execTernary();
 
     };
 };
