215f6d171b9e2dcad385c814ca995a1b891e1257
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-10-27 19:08:55 -0500

    switch case using Scanner class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-import static modules.module0.control_structures.Conditional.execIfElse;
-import static modules.module0.control_structures.Conditional.execTernary;
+import static modules.module0.control_structures.Conditional.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Holi mundo");
         execIfElse();
         execTernary();
+        execSwitchCase();
 
     };
 };
--- src/modules/module0/control_structures/Conditional.java
+++ src/modules/module0/control_structures/Conditional.java
@@ -1,4 +1,5 @@
 package modules.module0.control_structures;
+import java.util.Scanner;
 
 public class Conditional{
     public static void execIfElse(){
@@ -16,4 +17,50 @@ public class Conditional{
                 : "its not pair";
         System.out.println(result + " the numbar");
         };
+    public static void execSwitchCase() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número del 0 al 9: ");
+        int number = scanner.nextInt();
+
+        String numberInWords;
+        switch (number) {
+            case 0:
+                numberInWords = "cero";
+                break;
+            case 1:
+                numberInWords = "uno";
+                break;
+            case 2:
+                numberInWords = "dos";
+                break;
+            case 3:
+                numberInWords = "tres";
+                break;
+            case 4:
+                numberInWords = "cuatro";
+                break;
+            case 5:
+                numberInWords = "cinco";
+                break;
+            case 6:
+                numberInWords = "seis";
+                break;
+            case 7:
+                numberInWords = "siete";
+                break;
+            case 8:
+                numberInWords = "ocho";
+                break;
+            case 9:
+                numberInWords = "nueve";
+                break;
+            default:
+                numberInWords = "Número fuera del rango 0-9";
+                break;
+        }
+
+        System.out.println("El número ingresado en palabras es: " + numberInWords);
     }
+
+}
