943b6d2c8e3a136789f51f816044e4a08bacfda3
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-10-30 16:31:39 -0500

    chess pieces

---
+++ src/modules/module1/homework/Main.java
@@ -0,0 +1,4 @@
+package modules.module1.homework;
+
+public class Main {
+}
---
+++ src/modules/module1/homework/Rook.java
@@ -0,0 +1,13 @@
+package modules.module1.homework;
+
+public class Rook extends Piece {
+    public Rook(String color) {
+        super(color);
+    }
+
+    @Override
+    public boolean isValidMove(int startX, int startY, int endX, int endY) {
+        // Not implemented yet.
+        return true;
+    }
+}
