e3f02379d56923191861a387107d8bbe65366dd8
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-11-01 15:37:46 -0500

    finish string ops

--- src/modules/module1/intermediateConcepts/StringOperations.java
+++ src/modules/module1/intermediateConcepts/StringOperations.java
@@ -10,12 +10,54 @@ public class StringOperations {
 
         System.out.print("Por favor, ingresa una cadena: ");
         String input = scanner.nextLine();
-
+        // verify
         System.out.println("Ingresaste: " + input);
         System.out.println("A continuacion algunos datos: ");
+        //longitud de cadena.
         System.out.println("longitud de cadena: " + input.length());
+        // deteccion de palabras
+        System.out.println((input.indexOf("de")>0)?"exists":"doesnt estists");
+        // conversion de case
 
-        System.out.println(Arrays.toString(input.split("")));
+        String[] caracteres = input.split("");
+
+        for (int i = 0; i < caracteres.length; i++) {
+            String letra = caracteres[i];
+            if (letra.equals(letra.toLowerCase())) {
+                caracteres[i] = letra.toUpperCase();
+            } else {
+                caracteres[i] = letra.toLowerCase();
+            }
+        }
+        String str = String.join("", caracteres);
+        System.out.println(str);
+
+        // substring de la primera palabra:
+
+        char space = ' ';
+
+        int posicion = -1;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == space) {
+                posicion = i;
+                break;
+            }
+        }
+
+        String substring = input.substring(0,posicion);
+        System.out.println(substring);
+        // agregar palabas a la fraase
+        String phrase = "Hola mundirijillo.";
+        String ask = ", como te va?";
+        String bye = " Chao con adios";
+        int pos1 = phrase.indexOf("mundirijillo") + "mundirijillo".length();
+        String parte1 = phrase.substring(0, pos1);
+        String parte2 = phrase.substring(pos1);
+
+        String newPhrase = parte1 + ask + parte2 + bye;
+        System.out.println(newPhrase);
+        //
 
 
         scanner.close();
