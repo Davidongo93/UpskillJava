5f93f5300a1baabd623d7105b98a62545c09d97d
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-11-01 16:32:01 -0500

    interfaces exercise

---
+++ src/modules/module1/intermediateConcepts/Interfaces.java
@@ -0,0 +1,47 @@
+package modules.module1.intermediateConcepts;
+
+public class Interfaces {
+
+
+    public static class producto {
+       private String name;
+        private Double price;
+        private String code;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Double getPrice() {
+            return price;
+        }
+
+        public void setPrice(Double price) {
+            this.price = price;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public producto(String name) {
+            this.name = name;
+        }
+
+        public producto(String name, Double price, String code) {
+            this.name = name;
+            this.price = price;
+            this.code = code;
+        }
+    }
+
+
+}
