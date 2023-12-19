3aafe0c786758b9c2b3a6dcf8895213ce8aad7f3
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-12-19 17:11:11 -0500

    Scanner solved by args

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,10 +2,14 @@ package Main;
 
 import presentation.MenuLayer;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         //console CLI
 
-        MenuLayer.ShowMenu();
+        MenuLayer.showMenu(scanner);
+        scanner.close();
     }
 };
